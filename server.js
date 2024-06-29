f4e76b136d0929d60165eb7533e7c9ec9b348650
Hriday008 <hriday.sarma@globalnodes.ai>
2024-06-29 22:57:41 +0530

    Cors check - 1

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ import cors from "cors"; // Import the cors package
 // Assign app to
 const app = express(); // Initialize the Express application
 
-const corsOptions = {
-  origin: "*", // Allow all origins
-  credentials: true, // Access-Control-Allow-Credentials: true
-  optionsSuccessStatus: 200, // Status code for successful OPTIONS request
-};
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
 // Middleware setup
 app.use(json()); // Middleware to parse JSON bodies from incoming requests
