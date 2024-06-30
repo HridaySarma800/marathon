83cf6b437d97864f4b869f878ef34ef84e4d4b6a
Hriday008 <hriday.sarma@globalnodes.ai>
2024-06-30 18:37:27 +0530

    Test 2

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ import authRoutes from "./routes/authRoutes.js";
 import appRoutes from "./routes/appRoutes.js";
 import config from "./config/index.js";
 import { errorHandler } from "./middleware/errorHandler.js";
-import cors from "cors"; // Import the cors package
 
 // Assign app to
 const app = express(); // Initialize the Express application
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", [
+    "test.bytesbeyond.in",
+    "localhost:3000",
+    "bytesbeyond.in",
+  ]);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
